039a1ff58173cb676f77696cb1cb76389a330022
Sambhaji-Patil <sampat20th@gmail.com>
2024-11-05 18:37:21 +0530

    update5

--- detect_spam.py
+++ detect_spam.py
@@ -13,3 +13,4 @@ is_spam = model.predict([comment_text])[0]
 # Output result
 with open("is_spam_output.json", "w") as f:
     f.write(f'{{"is_spam": {is_spam}}}')
+print(f'Output written: {{"is_spam": {is_spam}}}')  # Add this line for debugging
